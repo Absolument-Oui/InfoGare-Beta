9e6a6b3d8820c3e1de12de7b1e94877ac39d5a8e
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2021-09-03 14:44:25 +0200

    BETA 8.8  - Début de la vréation des écrans quais  - Corrections des bugs empéchant le bon référencement du site

--- scripts/acces_quai.js
+++ scripts/acces_quai.js
@@ -1,1 +1,20 @@
 const database = firebase.database().ref('users');
+
+function loadQuais(userid, pairId, gid) {
+    database.child(userid).child('gares').child(gid).child('trains').get().then((snapshot) => {
+        var quais = new Array();
+        snapshot.forEach((child) => {
+            if (child.val().pairid === pairId) {
+                quais.push({
+                    id: child.key,
+                    number: child.val().number,
+                    destination: child.val().destination,
+                    hour: child.val().hourdepart,
+                    retard: child.val().ret,
+                    type: child.val().type,
+                    voie: child.val().voie
+                })
+            }
+        })
+    })
+}
