dbeee435efed9d7dc967b064fc4345ff87ce2d9c
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2021-09-11 00:29:04 +0200

    BETA 9 Création du nouveau système d'authentification

--- scripts/login.js
+++ scripts/login.js
@@ -5,13 +5,13 @@ var user = undefined;
 var SecretCode = null;
 
 document.getElementById('login_btn').onclick = function() {
-  window.location.href = 'login.htm?redirect=' + encodeURIComponent(window.location.href);
+  window.location.href = 'https://auth.infogare.fr/login.htm?returnurl=' + encodeURIComponent(window.location.href);
 }
 
 function loginWithToken(token) {
   firebase.auth().signInWithCustomToken(token).then((user) => {
     console.log(user.user.displayName);
-    //window.location.href = location.hostname + location.pathname;
+    window.location.href = location.hostname + location.pathname;
   })
 }
 
