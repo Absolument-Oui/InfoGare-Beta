4d6971a9f4c32560261f5ecd788fc9afaae4df7b
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2021-10-02 09:50:03 +0200

    BETA 10.0.8 Ajout de la photo de profil dans la barre de navigation

--- scripts/login.js
+++ scripts/login.js
@@ -22,6 +22,11 @@ function checkLogin() {
         document.getElementById('mnu_username').innerText = user.displayName;
         document.getElementById('mnu_users').hidden = false;
         document.getElementById('mnu_logout').hidden = false;
+        if (user.photoURL !== undefined) {
+          document.getElementById('mnu_user_photo').src = user.photoURL;
+          document.getElementById('mnu_user_photo').style.display = 'block';
+          document.getElementById('mnu_user_no_photo').style.display = 'none';
+        }
         if (location.host === 'beta.infogare.fr') {
           checkBeta(user.uid);
         }
