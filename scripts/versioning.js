73b701e5e9f4b2fb4ae57b7628154f7f1f9612ac
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2021-10-12 21:56:03 +0200

    BETA 10.0.13  - Ajout d'un indicateur de version en bas de page

---
+++ scripts/versioning.js
@@ -0,0 +1,13 @@
+const COMMITS_API = "https://api.github.com/repos/Absolument-Oui/InfoGare-Beta/commits";
+
+function getVersion() {
+    var req = new XMLHttpRequest();
+    req.open('GET', COMMITS_API);
+    req.onload((e) => {
+        var response = JSON.parse(req.response);
+        var message = response[0].message.split('\n');
+
+        document.getElementById('version').innerText = message[0];
+    });
+    req.send('');
+}
