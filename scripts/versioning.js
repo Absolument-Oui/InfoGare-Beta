df46e48c01c65eb609e46a9012a7d8f9944157dc
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2021-11-06 10:42:49 +0100

    BETA 10.1.13  - Ajustemtent de la page "versions.htm"

--- scripts/versioning.js
+++ scripts/versioning.js
@@ -1,8 +1,8 @@
-const COMMITS_API = "https://api.github.com/repos/Absolument-Oui/InfoGare-Beta/commits";
+const COMMITS_API = "https://api.github.com/repos/Absolument-Oui/InfoGare-Beta/commits?per_page=1000";
+var req = new XMLHttpRequest();
 
 function getVersion() {
-    var req = new XMLHttpRequest();
-    req.open('GET', COMMITS_API);
+    req.open('GET', COMMITS_API, false);
     req.onload = ((e) => {
         var response = JSON.parse(req.response);
         var item = response[0];
@@ -16,25 +16,21 @@ function getVersion() {
 }
 
 function getAllVersions() {
-    var req = new XMLHttpRequest();
-    req.open('GET', COMMITS_API);
-    req.onload = ((e) => {
-        var response = JSON.parse(req.response);
-        response.forEach(element => {
-            let div = document.createElement('div');
-            let h1 = document.createElement('h1');
-            let p = document.createElement('p');
+    var response = JSON.parse(req.response);
+    for(let i = 0; i < response.length; i++) {
+        let div = document.createElement('div');
+        let h1 = document.createElement('h1');
+        let p = document.createElement('p');
 
-            var title = element.commit.message.split('\n')[0];
-            var message = element.commit.message.slice(0, title.length - 1);
-            var commit_date = new Date(element.commit.author.date);
+        var title = response[i].commit.message.split('\n')[0];
+        var message = response[i].commit.message.slice(title.length, response[i].commit.message.length);
+        var commit_date = new Date(response[i].commit.author.date);
 
-            h1.innerText = `${message[0]} (${commit_date})`;
-            p.innerText = message;
-            div.appendChild(h1);
-            div.appendChild(p);
+        h1.innerText = `${title} (${commit_date.toLocaleDateString()})`;
+        p.innerText = message;
+        div.appendChild(h1);
+        div.appendChild(p);
 
-            document.getElementById('versions').appendChild(div);
-        });
-    });
+        document.getElementById('versions').appendChild(div);
+    };
 }
