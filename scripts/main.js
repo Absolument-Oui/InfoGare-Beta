c6b91b588e30fff39bb6a93dd992aa2ac3e016a0
FloYoutube54 <48404856+FloYoutube54@users.noreply.github.com>
2022-01-29 12:38:36 +0100

    BETA 11.2.3

--- scripts/main.js
+++ scripts/main.js
@@ -750,6 +750,36 @@ function createTrain() {
         from = from + y.options[j].value + "|";
     }
 
+    var days = new Array(7);
+
+    if (document.getElementById('train_days_monday').checked) {
+        days.push('1');
+    }
+
+    if (document.getElementById('train_days_tuesday').checked) {
+        days.push('2');
+    }
+
+    if (document.getElementById('train_days_wednesday').checked) {
+        days.push('3');
+    }
+
+    if (document.getElementById('train_days_thursday').checked) {
+        days.push('4');
+    }
+
+    if (document.getElementById('train_days_friday').checked) {
+        days.push('5');
+    }
+
+    if (document.getElementById('train_days_saturday').checked) {
+        days.push('6');
+    }
+
+    if (document.getElementById('train_days_sunday').checked) {
+        days.push('7');
+    }
+
     // Verification
 
     if (document.getElementById('train_dest').value === "" && document.getElementById('train_prov').value === "") {
@@ -788,7 +818,8 @@ function createTrain() {
         alternance: document.getElementById('train_dynamic').value,
         hall: document.getElementById('train_hall').value,
         alternancetype: infodynatype,
-        compo: compo_list
+        compo: compo_list,
+        days: days.toString()
     }).then((snapshot) => {
         document.location.reload();
     }).catch((error) => {
