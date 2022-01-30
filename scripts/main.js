c4d4d2fc1e8c8fe29ff8d19f2d8ac271b0155526
FloYoutube54 <48404856+FloYoutube54@users.noreply.github.com>
2022-01-30 21:37:28 +0100

    BETA 11.2.27

--- scripts/main.js
+++ scripts/main.js
@@ -3462,7 +3462,7 @@ function checkDay() {
 
         days.forEach(element => {
             console.log(element, today + 1);
-            if (element !== undefined) {
+            if (element !== 'undefined') {
                 if (element !== today + 1) {
                     $(this).addClass('row-group-hidden');
                 }
