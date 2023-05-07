8a9fc57678a3aa0d427d03d0b750e38553ff9bb6
Le Stagiaire <f.goussot@outlook.com>
2023-05-07 19:16:51 +0200

    Merge V2.2.9

---
+++ src/components/ControlPanel.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import '../index.scss';
+
+class ControlPanel extends Component {
+    render() {
+        return (
+            <div className='center'>
+                <h1>Control Panel</h1>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        
+    }
+}
+
+export default ControlPanel;
